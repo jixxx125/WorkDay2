bc0d5f1739f9d65c5733bc5e3a2cad578e171113
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:26:24 -0500

    done

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,36 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim fullname As String
+        'Read the input from the user
+        fullname = txtFullName.Text
+
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
+        'Trim the input so as to remove leading spaces
+
+
+        'Find the Index of first name
+        Dim idxSpace As Integer
+        idxSpace = fullname.IndexOf(" ")
+
+        'Extract the first name
+        Dim firstName As String
+        firstName = fullname.Substring(0, idxSpace)
+
+        'Display the result to the user
+        txtFirstName.Text = firstName
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub txtFullName_TextChanged(sender As System.Object, e As System.EventArgs) Handles txtFullName.TextChanged
 
     End Sub
 End Class
